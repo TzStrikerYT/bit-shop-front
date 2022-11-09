3a7236288af0aa0f42ba420249b02f442e0f9854
TzStrikerYT <maicol.asus@gmail.com>
2022-11-08 21:30:03 -0500

    version bump

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -18,10 +18,27 @@ export class AdminComponent implements OnInit {
 
     let response = this.productService.getProducts()
 
-    response.subscribe((res) => {
-      this.productService.products = res
+    response.subscribe((res: any) => {
+      this.productService.products = res.data //[{}{}{}{}] arreglo de productos
+      console.log(res)
     })
 
+  }
+
+  orderByPrice(){
+
+    let products = this.productService.products || []
+    let sortedProducts;
+
+    console.log("Ejecucion")
+
+    if (products || products.length > 0){
+      sortedProducts = products.sort((a: any, b: any) => (a.price > b.price) ? -1 : 1)
+      this.productService.products = sortedProducts
+      return
+    }
+
+    return 
   }
 
 }
