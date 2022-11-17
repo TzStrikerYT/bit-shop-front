a02f339de1f0a64e4fddf51f11ea52de514ef134
TzStrikerYT <maicol.asus@gmail.com>
2022-11-16 21:36:57 -0500

    [add] add logic for login

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  login(form: NgForm){
+
+    let {email, password} = form.value
+
+    if(!email || !password){
+      alert("Diligencie todos los campos")
+      return
+    }
+
+    this.userService.login(email, password).subscribe(
+      (res) => {
+        alert("Usuario logueado correctamente")
+        this.userService.userLogged = "token desencriptado"
+        // proteger las vistar contra los usuarios que no esten logueados
+        //mostrar los datos en la barra de navegación
+      },
+      (err) => {
+        alert(err.error.msg)
+      }
+    )
+
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,13 +9,19 @@ export class UserService {
 
   URL_API = "http://localhost:5000/user"
   currentUser: User
+  userLogged: any
 
   constructor(public http: HttpClient) { 
     this.currentUser = new User()
+    this.userLogged = null
   }
 
   signUp(data: any){
     return this.http.post(`${this.URL_API}/signup`, data)
+  }
+
+  login(email: string, password: string){
+    return this.http.post(`${this.URL_API}/login`, {email, password})
   }
 
 }
